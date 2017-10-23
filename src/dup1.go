7c4ce6f2632e89f32549394f632dd9a814a1e0f7
schintakinditesla <32309139+schintakinditesla@users.noreply.github.com>
2017-10-23 14:58:38 -0700

    new_progs

---
+++ src/dup1.go
@@ -0,0 +1,23 @@
+// DUP1 Program to print text of each line that appears more than once in the standard input, preceeded by its count.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
--- src/prog_1.go
+++ src/prog_1.go
@@ -1,13 +1,16 @@
+//modified this part of code in emacs :D
 package main
+
 import (
-"fmt"
+	"fmt"
 )
+
 /*import (
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 */
 
-func main(){
+func main() {
 	fmt.Println("Program_1:  including some mongodb packages into header which are successful but golang will complain if packages are imported and not used so commented them out. :D")
 }
